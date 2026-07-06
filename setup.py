636955ea8a2079ef1e6f2fb8e2ba49fb378588d1
ZCban <100644497+ZCban@users.noreply.github.com>
2026-07-06 15:39:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, Extension
 import pybind11
 
-ext_modules = [
-    Extension(
-        'gpu_capture',
-        ['main.cpp'],
-        include_dirs=[pybind11.get_include()],
-        language='c++',
-        extra_compile_args=['/O2', '/std:c++17'], 
-        libraries=['ole32', 'user32', 'd3d11', 'dxgi', 'd2d1', 'dxguid']
-    ),
-]
+gpu_capture_module = Extension(
+    "gpu_capture",
+    sources=["main.cpp"],
+    include_dirs=[pybind11.get_include()],
+    libraries=["d3d11", "dxgi", "d2d1", "user32", "ole32", "d3dcompiler"],
+    extra_compile_args=["/O2", "/std:c++17"],
+    language="c++"
+)
 
 setup(
-    name='gpu_capture',
-    ext_modules=ext_modules,
+    name="gpu_capture",
+    version="1.0",
+    ext_modules=[gpu_capture_module],
+    setup_requires=["pybind11"],
 )
